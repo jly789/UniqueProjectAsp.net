aa89d077d54550eec25742a5b6f8906887e2f85d
jly78 <jly789@naver.com>
2024-07-29 13:03:38 +0900

    초기 커밋.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Unique.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
